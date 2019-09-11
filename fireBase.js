6d28f88c9cdddccced94067f93ab0a9289a676b2
rockwellr2 <39406684+rockwellr2@users.noreply.github.com>
2019-09-10 21:40:49 -0400

    Add files via upload

---
+++ fireBase.js
@@ -0,0 +1,19 @@
+var app_fireBase = {};
+(function() {
+//	<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBJVcn2QbCsIhWnrRFr5-gFhoDlmTURW0c",
+    authDomain: "wheresthebus-3fa70.firebaseapp.com",
+    databaseURL: "https://wheresthebus-3fa70.firebaseio.com",
+    projectId: "wheresthebus-3fa70",
+    storageBucket: "",
+    messagingSenderId: "289932661582",
+    appId: "1:289932661582:web:f977c2f72f7d6f9b03041f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  app_fireBase = firebase;
+//</script>
+	})()
